4974d0890b27db3669a99b9e9f3f4bdc852d8eb0
tearth <Tearth@users.noreply.github.com>
2018-10-17 19:37:54 +0200

    Add floppy loader.

---
+++ FAT12 Viewer/FloppyLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FAT12Viewer
+{
+    public class FloppyLoader
+    {
+        public byte[] Load(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+    }
+}
