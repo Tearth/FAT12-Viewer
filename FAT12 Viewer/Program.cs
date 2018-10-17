083c88fe3d86c1493e9a98d19f5250fa0ec9eddd
tearth <Tearth@users.noreply.github.com>
2018-10-17 18:55:32 +0200

    Add initial project.

---
+++ FAT12 Viewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FAT12_Viewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
